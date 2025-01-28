2345cb0ea783dae482de7a5e361184c63c2078d8
arthurvera <arthurvera96@gmail.com>
2025-01-28 11:22:40 +0100

    add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Dashboard from "./components/Dashboard";
 import Layout from "./layouts/MainLayout";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <Layout>
       <Dashboard />
+      <Analytics />
     </Layout>
   );
 }
